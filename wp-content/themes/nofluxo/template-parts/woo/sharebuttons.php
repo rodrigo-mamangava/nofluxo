710b95cb06aed82d5d7580a0e915ac3e6df9ea5e
rodrigo-mamangava <rodrigo@mamangava.com>
2015-11-24 20:27:03 -0200

    beta1.0.6
    - ajustes para deploy
    - removi plugins problemáticos
    - share buttons da produto single funcionando

--- wp-content/themes/nofluxo/template-parts/woo/sharebuttons.php
+++ wp-content/themes/nofluxo/template-parts/woo/sharebuttons.php
@@ -10,21 +10,33 @@
         <a 
             class="btn btn-social-produto" 
             href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&amp;t=<?php the_title(); ?>" 
+            target="_blank"
             title="Share on Facebook.">
             <i class="fa fa-facebook"></i>
         </a>
         <a href="http://twitter.com/home/?status=<?php the_title(); ?> - <?php the_permalink(); ?>" 
-           title="Tweet this!" 
+           title="Tweet this!"
+           target="_blank"
            class="btn btn-social-produto">
             <i class="fa fa-twitter"></i>
         </a>
-        <a href="mailto:?subject=No Fluxo / <?php the_title(); ?>&body=<?php the_title()?>  -  <?php the_permalink() ?>" class="btn btn-social-produto">
+        <a 
+            href="mailto:?subject=No Fluxo / <?php the_title(); ?>&body=<?php the_title()?>  -  <?php the_permalink() ?>" 
+            target="_blank"
+            class="btn btn-social-produto">
             <i class="fa fa-envelope-o"></i>
         </a>
-        <a href="http://pinterest.com/pin/create/button/?url=<?php the_permalink(); ?>&media=<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); echo $url; ?>" class="btn btn-social-produto">
+        <a 
+            href="http://pinterest.com/pin/create/button/?url=<?php the_permalink(); ?>&media=<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); echo $url; ?>" 
+            target="_blank"
+            class="btn btn-social-produto">
             <i class="fa fa-pinterest"></i>
         </a>
-        <a href="https://plus.google.com/share?url=<?php the_permalink(); ?>" onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;" class="btn btn-social-produto">
+        <a 
+            href="https://plus.google.com/share?url=<?php the_permalink(); ?>" 
+            onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;" 
+            target="_blank"
+            class="btn btn-social-produto">
             <i class="fa fa-google-plus"></i>
         </a>                                    
     </div>
